6d9c1073d5750661e4af068e13361c7313f9e179
ranakh23 <42887031+ranakh23@users.noreply.github.com>
2020-12-04 23:34:24 +0200

    Create 11-the_western_exchange.py

---
+++ math/0x00-linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Implement np_transpose function"""
+
+
+def np_transpose(matrix):
+    """
+        Get the transpose of a matrix using numpy
+        Return: new numpy.ndarray
+    """
+    import numpy as np
+    return np.transpose(matrix)
