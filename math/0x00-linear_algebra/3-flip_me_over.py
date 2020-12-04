4938e5996d568fe2e34de060aa37839e64b49cd1
ranakh23 <ranakhoury23@gmail.com>
2020-12-04 20:14:27 +0000

    flip me over

---
+++ math/0x00-linear_algebra/3-flip_me_over.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"get transpose"
+
+
+def matrix_transpose(matrix):
+    "fun fun function"
+    mat=[]
+    for j in range(len(matrix[0])):
+        col=[]
+        for i in matrix:
+            col.append(i[j])
+        mat.append(col)
+    return mat
