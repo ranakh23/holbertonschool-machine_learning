7d4b65fb491421c790aaeb08896a287e1f61de17
ranakh23 <ranakhoury23@gmail.com>
2020-12-04 19:39:13 +0000

    fix pep8

--- math/0x00-linear_algebra/2-size_me_please.py
+++ math/0x00-linear_algebra/2-size_me_please.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python3
+"matrix shape"
+
 
 def matrix_shape(matrix):
-    shape=[]
-    mat=matrix
+    "function"
+    shape = []
+    mat = matrix
     i = len(mat)
     while i > 0:
         shape.append(i)
-        mat=mat[0]
+        mat = mat[0]
         if isinstance(mat, list):
-            i=len(mat)
+            i = len(mat)
         else:
             i = 0
     return shape
