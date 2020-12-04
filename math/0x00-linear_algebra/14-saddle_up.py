3eb6ce39568bdf88c109b999f5260aa2d41780ae
ranakh23 <42887031+ranakh23@users.noreply.github.com>
2020-12-04 23:36:37 +0200

    Create 14-saddle_up.py

---
+++ math/0x00-linear_algebra/14-saddle_up.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Implement np_matmul function"""
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """
+        Perform matrix multiplication
+        mat1: numpy.ndarray
+        mat2: numpy.ndarray
+        Return: new numpy.ndarray
+    """
+    return np.matmul(mat1, mat2)
