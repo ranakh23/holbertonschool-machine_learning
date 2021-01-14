4daef3842f58a8573f68a9a068a72dd1b2766a8c
ranakh23 <42887031+ranakh23@users.noreply.github.com>
2021-01-14 20:08:54 +0200

    Update 16-deep_neural_network.py

--- supervised_learning/0x01-classification/16-deep_neural_network.py
+++ supervised_learning/0x01-classification/16-deep_neural_network.py
@@ -1,26 +1,33 @@
 #!/usr/bin/env python3
-"""
-Class DeepNeuralNetwork
-Defines a deep multiple layers neural network
-"""
+""" DeepNeuralNetwork """
 import numpy as np
-import matplotlib.pyplot as plt
 
 
 class DeepNeuralNetwork:
+    """ class DeepNeuralNetwork """
     def __init__(self, nx, layers):
-        if not isinstance(nx, int):
-            raise TypeError("nx must be a integer")
+        """ init for DeepNeuralNetwork """
+        if type(nx) != int:
+            raise TypeError('nx must be an integer')
         if nx < 1:
-            raise ValueError("nx must be positive")
-        if not isinstance(layers, list):
-            raise TypeError("layers must be a list of positive integers")
-        if min(layers) < 1:
-            raise TypeError("layers must be a list of positive integers")
-
+            raise ValueError('nx must be a positive integer')
+        if type(layers) != list:
+            raise TypeError('layers must be a list of positive integers')
+        if len(layers) == 0:
+            raise TypeError('layers must be a list of positive integers')
+        self.nx = nx
+        self.layers = layers
         self.L = len(layers)
         self.cache = {}
-        self.weights = {"W1": np.random.randn(layers[0], nx), "b1": np.zeros((layers[0], 1))}
-        for i in range(1, self.L):
-            self.weights[("W{}".format(i+1))] = np.random.randn(layers[i], layers[i-1]) * np.sqrt(2/layers[i-1])
-            self.weights[("b{}".format(i+1))] = np.zeros((layers[i], 1))
+        self.weights = {}
+        for ly in range(self.L):
+            if type(layers[ly]) != int or layers[ly] <= 0:
+                raise TypeError('layers must be a list of positive integers')
+            self.weights["b"+str(ly+1)] = np.zeros((layers[ly], 1))
+            if ly == 0:
+                heetal = np.random.randn(layers[ly], nx) * np.sqrt(2/nx)
+                self.weights["W"+str(ly+1)] = heetal
+            else:
+                factor = np.sqrt(2/layers[ly-1])
+                heetal = np.random.randn(layers[ly], layers[ly-1]) * factor
+                self.weights["W" + str(ly+1)] = heetal
