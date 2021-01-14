7d8dbee831fec9923f549aed0f7c2a9615da160c
ranakh23 <42887031+ranakh23@users.noreply.github.com>
2021-01-14 20:45:45 +0200

    Create 8-RMSProp.py

---
+++ supervised_learning/0x03-optimization/8-RMSProp.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" RMSProp Upgraded """
+import tensorflow as tf
+
+
+def create_RMSProp_op(loss, alpha, beta2, epsilon):
+    """ creates the training operation for a NN
+    in tensorflow using the RMSProp optimization algorithm
+    """
+    return (tf.train.RMSPropOptimizer(learning_rate=alpha,
+            decay=beta2, epsilon=epsilon).minimize(loss))
