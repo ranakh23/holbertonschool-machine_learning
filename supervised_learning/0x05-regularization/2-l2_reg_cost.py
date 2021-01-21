58dee8bdf245d1a8b169c2c5641813984ebce8bf
ranakh23 <42887031+ranakh23@users.noreply.github.com>
2021-01-21 23:18:14 +0200

    Update 2-l2_reg_cost.py

--- supervised_learning/0x05-regularization/2-l2_reg_cost.py
+++ supervised_learning/0x05-regularization/2-l2_reg_cost.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
-
-"""cost"""
+""" Tensorflow L2 Regularization Cost """
 import tensorflow as tf
 
 
 def l2_reg_cost(cost):
-    """cost"""
+    """ Cost with L2 regularization """
     return cost + tf.losses.get_regularization_losses()
