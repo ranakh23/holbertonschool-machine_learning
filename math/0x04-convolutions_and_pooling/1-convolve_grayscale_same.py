0fcc1c2bd14f7c89fbcf6693c560a25c35ca32fe
ranakh23 <42887031+ranakh23@users.noreply.github.com>
2021-01-28 23:12:00 +0200

    Update 1-convolve_grayscale_same.py

--- math/0x04-convolutions_and_pooling/1-convolve_grayscale_same.py
+++ math/0x04-convolutions_and_pooling/1-convolve_grayscale_same.py
@@ -5,23 +5,14 @@ import numpy as np
 
 def convolve_grayscale_same(images, kernel):
     """ Same convolution on grayscale images"""
-    m, h, w = images.shape
     kh, kw = kernel.shape
-    pad_h = (kh - 1) // 2
-    pad_w = (kw - 1) // 2
-    if kh % 2 == 0:
-        pad_h = kh // 2
-    if kw % 2 == 0:
-        pad_w = kw / 2
-    new_images = np.pad(images, ((0, 0), (pad_h, pad_h), (pad_w, pad_w)),
-                        mode='constant', constant_values=0)
-    conv_result = np.zeros((m, h, w))
-    img = np.arange(m)
-    for row in range(h):
-        for col in range(w):
-            conv_result[img, row, col] = np.sum(np.multiply(new_images[img,
-                                                            row:row + kh,
-                                                            col:col + kw],
-                                                            kernel),
-                                                axis=(1, 2))
-    return conv_result
+    m, imh, imw = images.shape
+    ph = max((kh - 1) // 2, kh // 2)
+    pw = max((kw - 1) // 2, kw // 2)
+    padded = np.pad(images, ((0,), (ph,), (pw,)))
+    ans = np.zeros((m, imh, imw))
+    for i in range(imh):
+        for j in range(imw):
+            ans[:, i, j] = (padded[:, i: i + kh, j: j + kw] *
+                            kernel).sum(axis=(1, 2))
+    return ans
