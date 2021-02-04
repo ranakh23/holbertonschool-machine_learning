87f01804c9a95fd36af1498af551cb9b8df94544
ranakh23 <42887031+ranakh23@users.noreply.github.com>
2021-02-04 21:51:41 +0200

    Update 5-lenet5.py

--- supervised_learning/0x07-cnn/5-lenet5.py
+++ supervised_learning/0x07-cnn/5-lenet5.py
@@ -1,36 +1,30 @@
 #!/usr/bin/env python3
-""" LeNet-5 (Keras) """
+"""Function that builds a modified version of
+the LeNet-5 architecture using tensorflow"""
+
 import tensorflow.keras as K
 
 
 def lenet5(X):
-    """ builds modified version of LeNet-5 architecture using keras """
-    conv_lay1 = K.layers.Conv2D(filters=6,
-                                kernel_size=(5, 5),
-                                padding='same',
-                                activation='relu',
-                                kernel_initializer='he_normal')(X)
-    pool_lay1 = K.layers.MaxPooling2D(pool_size=(2, 2),
-                                      strides=(2, 2))(conv_lay1)
-    conv_lay2 = K.layers.Conv2D(filters=16,
-                                kernel_size=(5, 5),
-                                padding='valid',
-                                activation='relu',
-                                kernel_initializer='he_normal')(pool_lay1)
-    pool_lay2 = K.layers.MaxPooling2D(pool_size=(2, 2),
-                                      strides=(2, 2))(conv_lay2)
-    flatten = K.layers.Flatten(input_shape=(28, 28))(pool_lay2)
-    full_lay3 = K.layers.Dense(units=120,
-                               activation='relu',
-                               kernel_initializer='he_normal')(flatten)
-    full_lay4 = K.layers.Dense(units=84,
-                               activation='relu',
-                               kernel_initializer='he_normal')(full_lay3)
-    softmax = K.layers.Dense(units=10,
-                             activation='softmax',
-                             kernel_initializer='he_normal')(full_lay4)
-    model = K.Model(inputs=x, outputs=softmax)
-    model.compile(optimizer='adam',
-                  loss='categorical_crossentropy',
+    """Function that builds a modified version of
+    the LeNet-5 architecture using tensorflow"""
+    conv1 = K.layers.Conv2D(6, kernel_size=(5, 5), padding='same',
+                            kernel_initializer='he_normal',
+                            activation='relu')(X)
+    maxpool1 = K.layers.MaxPool2D((2, 2), (2, 2))(conv1)
+    conv2 = K.layers.Conv2D(16, kernel_size=(5, 5), padding='valid',
+                            kernel_initializer='he_normal',
+                            activation='relu')(maxpool1)
+    maxpool2 = K.layers.MaxPool2D((2, 2), (2, 2))(conv2)
+    flatten = K.layers.Flatten()(maxpool2)
+    dense1 = K.layers.Dense(units=120, kernel_initializer='he_normal',
+                            activation='relu')(flatten)
+    dense2 = K.layers.Dense(units=84, kernel_initializer='he_normal',
+                            activation='relu')(dense1)
+    dense3 = K.layers.Dense(units=10, kernel_initializer='he_normal',
+                            activation='softmax')(dense2)
+    optimizer = K.optimizers.Adam()
+    model = K.Model(inputs=X, outputs=dense3)
+    model.compile(loss='categorical_crossentropy', optimizer=optimizer,
                   metrics=['accuracy'])
     return model
