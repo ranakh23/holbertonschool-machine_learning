7c34dfd0999364cf890843d7137c8feebb3f0878
ranakh23 <42887031+ranakh23@users.noreply.github.com>
2021-02-04 21:36:17 +0200

    Update 0-conv_forward.py

--- supervised_learning/0x07-cnn/0-conv_forward.py
+++ supervised_learning/0x07-cnn/0-conv_forward.py
@@ -13,7 +13,8 @@ def conv_forward(A_prev, W, b, activation, padding="same", stride=(1, 1)):
         xpad = int(W.shape[1] / 2)
         ypad = int(W.shape[0] / 2)
         input = np.pad(A_prev, ((0, 0), (ypad - 1 + W.shape[1] % 2, ypad),
-                               (xpad - 1 + W.shape[1] % 2, xpad), (0, 0)), 'constant')
+                                (xpad - 1 + W.shape[1] % 2, xpad), 
+                                (0, 0)), 'constant')
     bottom = input.shape[1] - W.shape[0] + 1
     right = input.shape[2] - W.shape[1] + 1
     output = np.ndarray((input.shape[0], int((bottom - 1) / stride[0] + 1),
@@ -24,8 +25,11 @@ def conv_forward(A_prev, W, b, activation, padding="same", stride=(1, 1)):
         x_in = 0
         x_out = 0
         while x_in < right:
-            mulres = W[np.newaxis, ...] * input[:, y_in:y_in + W.shape[0], x_in:x_in + W.shape[1], :, np.newaxis]
-            output[:, y_out, x_out] = activation(mulres.sum(axis=(1, 2, 3)) + b)
+            mulres = W[np.newaxis, ...] * input[:, y_in:y_in + W.shape[0], 
+                                                x_in:x_in + W.shape[1], :,
+                                                np.newaxis]
+            output[:, y_out, x_out] = activation(mulres.sum(axis=(1, 2, 
+                                                                  3)) + b)
             x_in += stride[1]
             x_out += 1
         y_in += stride[0]
